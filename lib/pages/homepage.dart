d13cca64ed4a15c3b959d6ccb044b91a22f42348
kishore-klassy <kishorekumararcot@gmail.com>
2023-08-15 00:46:38 +0530

    designing new task page

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -3,10 +3,11 @@ import 'package:todoapp/components/todo_tile.dart';
 import 'package:todoapp/components/dialogbox.dart';
 import 'package:todoapp/data/database.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:todoapp/pages/new_task_page.dart';
 
 class HomePage extends StatefulWidget {
   final Box<dynamic> box;
-  const HomePage({super.key,required this.box});
+  const HomePage({super.key, required this.box});
 
   @override
   State<HomePage> createState() => _HomePageState();
@@ -15,10 +16,9 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final _controller = TextEditingController();
 
-    // reference the hive box
+  // reference the hive box
   final _myBox = Hive.box('mybox');
   TodoDataBase db = TodoDataBase();
-
 
   void checkBoxChanged(bool? value, int index) {
     setState(() {
@@ -40,46 +40,45 @@ class _HomePageState extends State<HomePage> {
   void deleteTask(int index) {
     setState(() {
       db.todoList.removeAt(index);
-
     });
-      db.updateDataBase();
+    db.updateDataBase();
   }
 
   void createNewTask() {
-     _controller.clear();
+    _controller.clear();
     showDialog(
         context: context,
         builder: (context) {
           return DialogBox(
-            controller: _controller,
-            onCancel: () => Navigator.of(context).pop(),
-            onSave: saveNewTask
-          );
+              controller: _controller,
+              onCancel: () => Navigator.of(context).pop(),
+              onSave: saveNewTask);
         });
-
   }
-void editTask(String taskName) {
-  _controller.text = taskName;
-  showDialog(
-    context: context,
-    builder: (context) {
-      return DialogBox(
-        controller: _controller,
-        onCancel: () => Navigator.of(context).pop(),
-        onSave: () {
-          int index = db.todoList.indexWhere((task) => task[0] == taskName);
-          if (index != -1) {
-            setState(() {
-              db.todoList[index][0] = _controller.text;
-            });
-            _controller.clear();
-            Navigator.of(context).pop();
-          }
-        },
-      );
-    },
-  );
-}
+
+  void editTask(String taskName) {
+    _controller.text = taskName;
+    showDialog(
+      context: context,
+      builder: (context) {
+        return DialogBox(
+          controller: _controller,
+          onCancel: () => Navigator.of(context).pop(),
+          onSave: () {
+            int index = db.todoList.indexWhere((task) => task[0] == taskName);
+            if (index != -1) {
+              setState(() {
+                db.todoList[index][0] = _controller.text;
+              });
+              _controller.clear();
+              Navigator.of(context).pop();
+            }
+          },
+        );
+      },
+    );
+  }
+
   @override
   void initState() {
     // if this is the 1st time ever opening the app, then create default data
@@ -93,39 +92,50 @@ void editTask(String taskName) {
     super.initState();
   }
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[200],
-        floatingActionButton: FloatingActionButton(
-          onPressed: createNewTask,
-          child: const Icon(Icons.add),
+      backgroundColor: Color.fromARGB(255, 52, 53, 67),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: const Icon(Icons.add),
+      ),
+      appBar: AppBar(
+        title: const Text(
+          "TO DO",
+          style: TextStyle(fontWeight: FontWeight.w500),
         ),
-        appBar: AppBar(
-          title: const Text(
-            "TO DO",
-            style: TextStyle(fontWeight: FontWeight.w500),
+        backgroundColor: Colors.lightBlueAccent,
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+                child: db.todoList.length == 0
+                    ? const Center(child: Text("No Task,Add Some thing"))
+                    : ListView.builder(
+                        itemCount: db.todoList.length,
+                        itemBuilder: (context, index) {
+                          return TodoTile(
+                            taskName: db.todoList[index][0],
+                            taskCompleted: db.todoList[index][1],
+                            onChanged: (value) => checkBoxChanged(value, index),
+                            deleteFunction: (context) => deleteTask(index),
+                            editFunction: editTask,
+                          );
+                        },
+                      )),
           ),
-          backgroundColor: Colors.lightBlueAccent,
-        ), 
-        body: Container(
-         child: db.todoList.length==0 ?
-            const Center(child: Text("No Task,Add Some thing"))
-            : ListView.builder(
-            itemCount: db.todoList.length,
-            itemBuilder: (context, index) {
-              return TodoTile(
-                  taskName: db.todoList[index][0],
-                  taskCompleted: db.todoList[index][1],
-                  onChanged: (value) => checkBoxChanged(value, index),
-                  deleteFunction: (context)=>deleteTask(index),
-                  editFunction:editTask ,
-                  );
-            },
-          )
-        ),
-         
-        );
+          MaterialButton(
+              onPressed: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => NewTaskPage(controller: _controller,))),
+                  color: Colors.blue,
+                       child: Text("Create New Task"),
+      
+                  
+                  )
+        ],
+      ),
+    );
   }
 }
--- lib/pages/new_task_page.dart
+++ lib/pages/new_task_page.dart
@@ -1,7 +1,15 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:todoapp/pages/homepage.dart';
+import 'package:todoapp/main.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class NewTaskPage extends StatefulWidget {
-  const NewTaskPage({super.key});
+  final TextEditingController controller;
+  const NewTaskPage({super.key, required this.controller});
 
   @override
   State<NewTaskPage> createState() => _NewTaskPageState();
@@ -10,6 +18,189 @@ class NewTaskPage extends StatefulWidget {
 class _NewTaskPageState extends State<NewTaskPage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: SafeArea(
+        child: Container(
+            color: Color.fromARGB(255, 52, 53, 67),
+            child:
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton(
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Colors.white,
+                        size: 30,
+                      ),
+                      onPressed: () => Navigator.of(context).pop()),
+                  Text(
+                    "Todays Task",
+                    style: GoogleFonts.aBeeZee(
+                        color: Colors.white,
+                        fontSize: 25,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  // Container(
+                  //   child: Lottie.network("https://lottie.host/?file=3183d921-6407-4e35-8889-bc1ced558933/6BjNGjz1Pb.json",
+                  //       width: 20, height: 20),
+                  // ),
+                  Icon(
+                    Icons.account_circle_rounded,
+                    color: Colors.white,
+                    size: 40,
+                  )
+                ],
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.only(top: 20),
+                  padding: EdgeInsets.only(left: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(30),
+                      topRight: Radius.circular(30),
+                    ),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 20, horizontal: 10),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Create",
+                                style: GoogleFonts.aBeeZee(
+                                    color: Colors.black,
+                                    fontSize: 30,
+                                    fontWeight: FontWeight.w600),
+                              ),
+                              Text("New Task",
+                                  style: GoogleFonts.aBeeZee(
+                                      color: Colors.black,
+                                      fontSize: 30,
+                                      fontWeight: FontWeight.w600)),
+                            ]),
+                      ),
+                      Container(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                        child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Task title",
+                                style: GoogleFonts.aBeeZee(
+                                    color: Colors.black,
+                                    fontSize: 20,
+                                    fontWeight: FontWeight.w700),
+                              ),
+                              TextField(
+                                controller: widget.controller,
+                                decoration: const InputDecoration(
+                                    hintText: "Add Task Name....",
+                                    hintStyle: TextStyle(
+                                        color: Colors.grey,
+                                        fontWeight: FontWeight.w600),
+                                    enabledBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide(
+                                            color: Color.fromARGB(
+                                                255, 239, 237, 237))),
+                                    fillColor:
+                                        Color.fromARGB(255, 239, 237, 237),
+                                    filled: true,
+                                    border: UnderlineInputBorder(
+                                        borderSide:
+                                            BorderSide(color: Colors.grey)),
+                                    focusedBorder: UnderlineInputBorder(
+                                        borderSide: BorderSide.none)),
+                              ),
+                            ]),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          "Category",
+                          style: GoogleFonts.aBeeZee(
+                              fontSize: 20, fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.symmetric(horizontal: 10),
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 239, 237, 237),
+                            borderRadius: BorderRadius.circular(12)),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 5),
+                                decoration: BoxDecoration(
+                                    color: Colors.blue,
+                                    borderRadius: BorderRadius.circular(12)),
+                                child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Icon(
+                                        Icons.person_outline_sharp,
+                                        size: 50,
+                                        color: Colors.white,
+                                      ),
+                                      Text(
+                                        "Personal",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 20),
+                                      )
+                                    ]),
+                              ),
+                            ),
+                            SizedBox(
+                              width: 20,
+                            ),
+                            Expanded(
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 10, vertical: 5),
+                                decoration: BoxDecoration(
+                                    color: Colors.grey[350],
+                                    borderRadius: BorderRadius.circular(12)),
+                                child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceEvenly,
+                                    children: [
+                                      Icon(
+                                        Icons.groups_2,
+                                        size: 50,
+                                        color: Colors.white,
+                                      ),
+                                      Text(
+                                        "Teams",
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 20),
+                                      )
+                                    ]),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ])),
+      ),
+    );
   }
 }
